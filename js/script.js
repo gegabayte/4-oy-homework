d1b64b07e9047597e3d5a120f483a01d50223ca8
gegabayte <xasanboymuhammaddinov41@gmail.com>
2024-01-16 23:59:40 +0500

    changed

---
+++ js/script.js
@@ -0,0 +1,88 @@
+// 1-misol
+// 3 ta son berilgan bular orasidan 3 ga karralilarning kopaytmasini topuvchi algoritm yozing.
+// let firstNumber = 7;
+// let secondNumber = 8;
+// let theredNumber = 9;
+// let res = ``
+
+// if (firstNumber % 3 == 0) {
+//     res = `firstNumber`
+// }
+// if (secondNumber % 3 == 0) {
+//     res = `secondNumber`
+// } 
+// if (theredNumber % 3 == 0) {
+//     res = `theredNumber`
+// } 
+// console.log(res)
+
+
+//2misol 
+//3 xonali son berilgan uning raqamlar yigindisini topadigan algoritm yozing.
+// let num = 140;
+// let firstNumber = num % 10;
+// let secondNumber = (num / 10) % 10;
+// let theredNumber = (num / 100);
+// let sum = firstNumber + secondNumber + theredNumber;
+// console.log(`3 xonali sonning raqamlari yig'indisi ${sum}`);
+
+
+//3-misol
+//3 xonalison berilgan uning toq raqamlari kopaytmasini topuvchi algoritm yozing.
+// let number = +prompt(`birinchi sonni kiriting`);
+// let res = ``;
+// let sum =`1`
+// if ( number % 2 == 1) {
+//     let = number % 10;
+//     sum = sum + number
+// } 
+// if ( number % 2 == 1) {
+//     res = (number / 10) % 10;
+//     sum = sum + number
+// } 
+// if ( number % 2 == 1) {
+//     res = (num / 100);
+//     sum = sum + number
+// } 
+//     console.log(sum)
+
+
+//4-misol
+//3ta som berilgan ular orasida  5ga hamda 7 ga  karralilarining sonnini topuvchi algoritm yozing
+// let firstNumber = +prompt(`1- sonni kiriting`);
+// let secondNumber = +prompt(`2- sonni kiriting`);
+// let theredNumber = +prompt(`3- sonni kiriting`);
+// let res = ``;
+// let counter = `0`;
+
+// if (firstNumber % 5 == 0 && firstNumber % 7 ==0) {
+//     res = `firstNumber`
+//     counter++;
+// };
+// if (secondNumber % 5 == 0 && secondNumber % 7 ==0) {
+//     res = `secondNumber`
+//     counter++;
+// };
+// if (theredNumber % 5 == 0 && theredNumber % 7 ==0) {
+//     res = `theredNumber`
+//     counter++;
+// };
+//     console.log(counter);
+
+
+//5-misol
+//ixtiyoriy harf  berilganda uning unli yoki undosh ekanini anqilovchi algoritm yozing
+// let search = +prompt(`ixtiyorish harf kiriting`)
+// if (search >= a && search <= A) {
+//     console.log('unli harf');
+// } else if (search >= u && search <=U) {
+//     console.log('unli harf');
+// } else if (search >= e && search <=E) {
+//     console.log('unli harf');
+// } else if (search >= o && search <=O) {
+//     console.log('unli harf');
+// } else if (search >= o' && search <=O') {
+//     console.log('unli harf');
+// }  else if  (search >= i && search <=I) {
+//     console.log('unli harf');
+// } 
